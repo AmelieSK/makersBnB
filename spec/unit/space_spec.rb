cd1e7e8abb36ada22b6caf668cdd199d2923f861
Amy <amee_sk@yahoo.com>
2020-10-24 13:23:32 +0100

    Space spec

--- spec/unit/space_spec.rb
+++ spec/unit/space_spec.rb
@@ -2,7 +2,10 @@ require './app/models/space'
 
 describe Space do
   it 'adds a new space' do
-    space = Space.add()
+    space = Space.create(name: 'Oasis', description: 'Relaxing accomodation with views of the city', price: '70')
+    expect(space.name).to eq 'Oasis'
+    expect(space.description).to eq 'Relaxing accomodation with views of the city'
+    expect(space.price).to eq 70
   end
 end
 
